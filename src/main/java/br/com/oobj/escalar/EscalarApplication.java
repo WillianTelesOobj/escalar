01c68a1d6c1ab68818ae0811c5fa666a94c4f4dc
Willian Teles <williantelesamb@gmail.com>
2022-07-20 17:01:44 -0300

    Primeiro commit

---
+++ src/main/java/br/com/oobj/escalar/EscalarApplication.java
@@ -0,0 +1,12 @@
+package br.com.oobj.escalar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscalarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscalarApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/oobj/escalar/EscalarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.oobj.escalar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscalarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
