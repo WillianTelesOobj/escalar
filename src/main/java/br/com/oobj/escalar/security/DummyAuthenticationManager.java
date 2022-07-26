b987aad4ba25443209b09eea67136ea7dbb966df
Willian Teles <williantelesamb@gmail.com>
2022-07-26 02:37:37 -0300

    Tratamento de Excecao

--- src/main/java/br/com/oobj/escalar/security/DummyAuthenticationManager.java
+++ src/main/java/br/com/oobj/escalar/security/DummyAuthenticationManager.java
@@ -5,8 +5,12 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 
 public class DummyAuthenticationManager implements AuthenticationManager {
+
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        if(authentication == null) {
+            throw new IllegalArgumentException("Token de autenticação não pode ser nulo!");
+        }
         return authentication;
     }
 }
